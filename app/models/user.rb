26806c29d27c0adceec27920b2597b49a4288788
masataka_omura <masataka.omura@icloud.com>
2020-12-17 14:00:08 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  # with_options precense: true do
+  #   validates :nickname
+  #   validates :last_name
+  #   validates :first_name
+  #   validates :last_name_kana, format: {
+  #     with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+  #     message: "全角カタカナのみで入力して下さい"
+  #   }
+  #   validates :first_name_kana, format: {
+  #     with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+  #     message: "全角カタカナのみで入力して下さい"
+  #   }
+  #   validates :birthday
+  # end  
 
 end
