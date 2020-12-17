a10e7a13a33e6087861d6a1067dc758e0456f9cd
masataka_omura <masataka.omura@icloud.com>
2020-12-17 14:52:04 +0900

    新規登録・ログイン・ログアウト機能
    バリデーションの修正もあります

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # with_options precense: true do
-  #   validates :nickname
-  #   validates :last_name
-  #   validates :first_name
-  #   validates :last_name_kana, format: {
-  #     with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
-  #     message: "全角カタカナのみで入力して下さい"
-  #   }
-  #   validates :first_name_kana, format: {
-  #     with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
-  #     message: "全角カタカナのみで入力して下さい"
-  #   }
-  #   validates :birthday
-  # end  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { 
+      with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, 
+      message: '全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end  
+    with_options format: {
+      with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+      message: "全角カタカナのみで入力して下さい"} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end  
+    
+  end
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字のみで入力して下さい"}  
 
 end
