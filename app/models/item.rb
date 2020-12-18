33942ba00a79c01d36698f17aa01c7f2e5ef6f6c
masataka_omura <masataka.omura@icloud.com>
2020-12-18 15:13:10 +0900

    shipping_fee完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
+  belongs_to :shipping_fee
 
   with_options presence: true, numericality: {other_than: 1} do
     validates :category_id
     validates :state_id
+    validates :shipping_fee_id
   end  
 end
