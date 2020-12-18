77c3d25ea76757dcd5c3d3e879db237e33f83350
masataka_omura <masataka.omura@icloud.com>
2020-12-18 15:40:02 +0900

    shipping_date完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,13 @@ class Item < ApplicationRecord
   belongs_to :state
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :shipping_date
 
   with_options presence: true, numericality: {other_than: 1} do
     validates :category_id
     validates :state_id
     validates :shipping_fee_id
     validates :prefecture_id
+    validates :shipping_date_id
   end  
 end
