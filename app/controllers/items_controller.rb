a446758e973ad1d51cff6ff08658f8172b524a5a
masataka_omura <masataka.omura@icloud.com>
2020-12-18 16:22:07 +0900

    ストロングパラメーター作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,11 @@ class ItemsController < ApplicationController
   end
   
   def create
+ 
   end  
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :category_id, :state_id, :shipping_fee_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
 end
