d950a5319aaf21ee3ba3ddf1654e502624e680ef
masataka_omura <masataka.omura@icloud.com>
2020-12-18 15:25:50 +0900

    prefecture完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :state
   belongs_to :shipping_fee
+  belongs_to :prefecture
 
   with_options presence: true, numericality: {other_than: 1} do
     validates :category_id
     validates :state_id
     validates :shipping_fee_id
+    validates :prefecture_id
   end  
 end
