1cecb76639d957513fbd852f576ac43bb7e10f9f
masataka_omura <masataka.omura@icloud.com>
2020-12-19 16:42:36 +0900

    アクセス制限修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,9 @@ class ItemsController < ApplicationController
   end
 
   def forbit_correct_user
-    params[:id] != current_user.id
-    redirect_to root_path
+    item = Item.find(params[:id])
+    if item.user.id != current_user.id
+      redirect_to root_path
+    end
   end  
 end
