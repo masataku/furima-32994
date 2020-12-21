bcd7d412fc5db10d051f963e6e4b24d60c067fba
masataka_omura <masataka.omura@icloud.com>
2020-12-21 19:42:10 +0900

    トークンの生成

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,12 +1,20 @@
 const pay = () => {
   Payjp.setPublicKey("pk_test_6e72a28837a18e5b3d04c5de");
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
+  const formDom = document.getElementById("charge-form");
+  formDom.addEventListener("submit", (e) => {
+    e.preventDefault();   
+    const formData = new FormData(formDom);
     const card = {
-      number: formData.get("")
-    }
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;  
+      }
+    });
   });
 };
 
