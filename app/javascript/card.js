cd5cd518de578b22e8444fe7c7d012524b6f43bf
masataka_omura <masataka.omura@icloud.com>
2020-12-21 18:12:59 +0900

    公開鍵を設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,9 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_6e72a28837a18e5b3d04c5de");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+
   });
 };
 
