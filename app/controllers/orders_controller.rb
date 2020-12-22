12624cb43bb743768b6f9d3df061c54a123cb9ab
masataka_omura <masataka.omura@icloud.com>
2020-12-22 13:14:18 +0900

    クレジット決済機能作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,29 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end  
 
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      item_pay
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'  
+    end  
+  end  
+
   private 
   def order_params
-    params.require(:order).permit().merge(token: params[:token])
+    params.require(:order).permit().merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end  
+
+  def item_pay
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    @item = Item.find(params[:item_id])
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end  
 end
