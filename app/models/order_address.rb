f336f3d1349e1b08592140b97a7329a726321ec9
masataka_omura <masataka.omura@icloud.com>
2020-12-23 18:57:03 +0900

    バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :prefecture_id, numericality: {other_than: 1}
     validates :city
     validates :address
-    validates :phone_number, numericality: {only_integer: true}
+    validates :phone_number, numericality: {only_integer: true}, length: {maximum: 11}
     validates :user_id
     validates :item_id
   end  
