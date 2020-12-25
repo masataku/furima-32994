bf70c8d67957e052e4dc8004d7d96b7e17b02f47
masataka_omura <masataka.omura@icloud.com>
2020-12-25 18:25:33 +0900

    create/destroyアクションの編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,20 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create 
+    @like = Like.build(user_id: current_user.id, item_id: params[:item_id])
+    respond_to do |format|
+      if @like.save
+        format.js 
+      end
+    end    
   end
   
   def destroy
+    @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
+    @like.destroy
+    @like = Like.new
+    respond_to do |format|
+      format.js
+    end  
   end  
 end
