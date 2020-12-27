37b641a6d655c1cbd98da1dc0376131f812677ca
masataka_omura <masataka.omura@icloud.com>
2020-12-27 21:10:15 +0900

    Commentモデルの単体テストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    text {"コメントです"}
+    association :user
+    association :item
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "コメント機能のテスト" do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+    context "コメントの投稿がうまくいく時" do
+      it "コメントのtextが空ではない" do
+        expect(@comment).to be_valid
+      end
+    end
+    context "コメントの投稿がうまくいかない時" do
+      it "コメントのtextが空" do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end  
+    end      
+  end  
 end
